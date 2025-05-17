69f2e9c0d48b9d36e2e2542d9fb337a874fb42f8
Strikeeaglechase <Strikeeaglechase@gmail.com>
2025-05-16 21:39:08 -0400

    Yes

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using K4os.Compression.LZ4;
+using System.Linq;
+
+namespace LZ4Decoder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Input file: {args[0]}");
+            Console.WriteLine($"Output file: {args[1]}");
+
+            var input = File.ReadAllBytes(args[0]);
+            var decompressBuffer = new byte[input.Length * 255];
+
+            int decompressSize = LZ4Codec.Decode(input, 0, input.Length, decompressBuffer, 0, decompressBuffer.Length);
+            Console.WriteLine($"Compressed size: {input.Length}, decompressed size: {decompressSize}");
+            File.WriteAllBytes(args[1], decompressBuffer.SkipLast(decompressBuffer.Length - decompressSize).ToArray());
+        }
+    }
+}
